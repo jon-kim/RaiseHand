30e44fb513733c24483e92fabf7160094cd387da
Jonathan Kim <jkim@ayakitchens.com>
2022-04-14 15:20:37 -0400

    Add project files.

---
+++ RaiseHand/Server/Hubs/RaiseHandHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RaiseHand.Server.Hubs
+{
+    public class RaiseHandHub : Hub
+    {
+        public static Dictionary<string, string> _users = new Dictionary<string, string>();   
+        public override async Task OnConnectedAsync()
+        {
+            var username = Context.GetHttpContext().Request.Query["username"];
+
+            _users.Add(Context.ConnectionId, username);
+
+            await AddMessageToChat(username, false);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+
+        }
+
+        public async Task AddMessageToChat(string user, bool raiseHand)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, raiseHand);
+
+        }
+    }
+}
