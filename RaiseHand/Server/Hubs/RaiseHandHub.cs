e962baf5ec7ba4c01ec506b5d7523a3eea5ca582
Jon Kim <email@jon.kim>
2022-04-17 20:50:09 -0400

    checkbox to show raised hands

--- RaiseHand/Server/Hubs/RaiseHandHub.cs
+++ RaiseHand/Server/Hubs/RaiseHandHub.cs
@@ -4,14 +4,14 @@ namespace RaiseHand.Server.Hubs
 {
     public class RaiseHandHub : Hub
     {
-        public static Dictionary<string, string> _users = new Dictionary<string, string>();   
+        public static Dictionary<string, string> _users = new Dictionary<string, string>();
         public override async Task OnConnectedAsync()
         {
             var username = Context.GetHttpContext().Request.Query["username"];
 
-            _users.Add(Context.ConnectionId, username);
+            _users[Context.ConnectionId] = username;
 
-            await AddMessageToChat(username, false);
+            await SetHandRaised(username, false);
             await base.OnConnectedAsync();
         }
 
@@ -20,9 +20,9 @@ namespace RaiseHand.Server.Hubs
 
         }
 
-        public async Task AddMessageToChat(string user, bool raiseHand)
+        public async Task SetHandRaised(string user, bool raiseHand)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, raiseHand);
+            await Clients.All.SendAsync("ReceiveHand", user, raiseHand);
 
         }
     }
