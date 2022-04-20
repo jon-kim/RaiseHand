b81fdeda5e0adab0402421b72aa073e9f2116ff8
Jon Kim <email@jon.kim>
2022-04-20 06:57:13 -0400

    ReceiveAllHands WIP

--- RaiseHand/Server/Hubs/RaiseHandHub.cs
+++ RaiseHand/Server/Hubs/RaiseHandHub.cs
@@ -1,29 +1,39 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using RaiseHand.Shared;
 
 namespace RaiseHand.Server.Hubs
 {
     public class RaiseHandHub : Hub
     {
-        public static Dictionary<string, string> _users = new Dictionary<string, string>();
+        //public static Dictionary<string, string> _users = new Dictionary<string, string>();
+        public static Dictionary<string, UserHand> _connections = new Dictionary<string, UserHand>();
         public override async Task OnConnectedAsync()
         {
             var username = Context.GetHttpContext().Request.Query["username"];
 
-            _users[Context.ConnectionId] = username;
+            //_users[Context.ConnectionId] = username;
+            _connections[Context.ConnectionId] = new UserHand(username);
 
-            await SetHandRaised(username, false);
+            //await SetHandRaised(username, false);
+            await SendAllHands();
             await base.OnConnectedAsync();
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
-            string username = _users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            //string username = _users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            string username = _connections.FirstOrDefault(u => u.Key == Context.ConnectionId).Value.Username;
             await RemoveHand(username);
         }
 
         public async Task SetHandRaised(string user, bool raiseHand)
         {
             await Clients.All.SendAsync("ReceiveHand", user, raiseHand);
+        }
+
+        public async Task SendAllHands()
+        {
+            await Clients.All.SendAsync("ReceiveAllHands", _connections.Values.ToArray());
         }
 
         public async Task RemoveHand(string user)
---
+++ RaiseHand/Shared/UserHand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaiseHand.Shared
+{
+    public struct UserHand
+    {
+        public UserHand() : this(string.Empty, false) { }
+        public UserHand(string username) : this(username, false) { }
+        public UserHand(string username, bool handRaised)
+        {
+            Username = username;
+            HandRaised = handRaised;
+        }
+
+        public string Username;
+        public bool HandRaised;
+    }
+}
